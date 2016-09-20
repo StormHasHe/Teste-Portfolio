61049a3936ca4145e6791c76b5f39f9070422b52
StormHasHe <caios.fontes@gmail.com>
2016-09-19 23:37:34 -0300

    alteracoes portfolio

--- Content/js/index.js
+++ Content/js/index.js
@@ -1,4 +1,28 @@
-﻿function sendEmail() {
+﻿var ownerGallery = [];
+
+ownerGallery.push("Content/images/portfolio/Owner/ownerLogo.jpg");
+ownerGallery.push("Content/images/portfolio/Owner/owner.jpg");
+ownerGallery.push("Content/images/portfolio/Owner/owner2.jpg");
+ownerGallery.push("Content/images/portfolio/Owner/owner3.jpg");
+ownerGallery.push("Content/images/portfolio/Owner/capa_face.jpg");
+
+var tpGallery = [];
+
+tpGallery.push("Content/images/portfolio/TenisPolar/divulgacao_facebook.png");
+tpGallery.push("Content/images/portfolio/TenisPolar/Feature Graphic.png");
+tpGallery.push("Content/images/portfolio/TenisPolar/mao2.png");
+tpGallery.push("Content/images/portfolio/TenisPolar/img_desc1.png");
+tpGallery.push("Content/images/portfolio/TenisPolar/img_desc2.png");
+tpGallery.push("Content/images/portfolio/TenisPolar/img_desc3.png");
+tpGallery.push("Content/images/portfolio/TenisPolar/img_desc4.png");
+
+var pequenosInfinitosGallery = [];
+
+pequenosInfinitosGallery.push("Content/images/portfolio/PequenosInfinitos/CAPA1.jpg");
+pequenosInfinitosGallery.push("Content/images/portfolio/PequenosInfinitos/CAPA2.jpg");
+pequenosInfinitosGallery.push("Content/images/portfolio/PequenosInfinitos/PEQINFINITOS-face2.jpg");
+
+function sendEmail() {
     var email = $("#txtEmail").val();
     var subject = $("#txtAssunto").val();
     var message = $("#txtMensagem").val();
@@ -27,15 +51,25 @@ function setAccordion(){
     }
 }
 
+function setScrollEvent() {
+    $(".divGalery li").bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($("#portfolioDetails").offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+}
+
 function loadDesktop(){
     var content = "" +
     "            <div class='accordion'>"+
     "                <ul>"+
-    "                    <li data-toggle='modal' data-target='#lightboxOwner'>"+
+    "                    <li onclick=loadGalleryDetails(&quot;Owner&quot;)>" +
     "                    </li>"+
-    "                    <li data-toggle='modal' data-target='#lightboxTP'>"+
+    "                    <li onclick=loadGalleryDetails(&quot;TenisPolar&quot;)>" +
     "                    </li>"+
-    "                    <li data-toggle='modal' data-target='#lightboxPI'>"+
+    "                    <li onclick=loadGalleryDetails(&quot;PequenosInfinitos&quot;)>" +
     "                    </li>"+
     "                </ul>"+
     "            </div>";
@@ -46,14 +80,92 @@ function loadDesktop(){
 function loadMobile(){
     var content = "" +
     "            <div class='images-slide'> "+
-    "                <a href='javascript:;'><img data-toggle='modal' data-target='#lightboxOwner' src='Content/images/portfolio/Owner/ownerLogo.jpg' /></a>"+
-    "                <a href='javascript:;'><img data-toggle='modal' data-target='#lightboxTP' src='Content/images/portfolio/TenisPolar/divulgacao_facebook.png' /></a>"+
-    "                <a href='javascript:;'><img data-toggle='modal' data-target='#lightboxPI' src='Content/images/portfolio/PequenosInfinitos/CAPA1.jpg' /></a>"+
+    "                <a href='javascript:;'><img onclick=loadGalleryDetails(&quot;Owner&quot;) src='Content/images/portfolio/Owner/ownerLogo.jpg' /></a>"+
+    "                <a href='javascript:;'><img onclick=loadGalleryDetails(&quot;TenisPolar&quot;) src='Content/images/portfolio/TenisPolar/divulgacao_facebook.png' /></a>"+
+    "                <a href='javascript:;'><img onclick=loadGalleryDetails(&quot;PequenosInfinitos&quot;) src='Content/images/portfolio/PequenosInfinitos/CAPA1.jpg' /></a>"+
     "            </div>";
 
     $(".divGalery").html(content);
 }
 
+function loadGalleryDetails(galleryName){
+
+    $("#portfolioDetails").fadeOut(500).promise().done(function(){
+        $(".divGaleryDetails").html("");
+
+        switch (galleryName){
+            case 'Owner':
+                var title = "Owner Wearing";
+                var description = "Projeto feito para uma nova loja de roupas recem lançada.";
+                loadGalleryTitleAndDescription(title, description);
+                loadGalleryImages(ownerGallery);
+                break;
+            case 'TenisPolar':
+                var title = "Tenis Polar";
+                var description = "Aplicativo lançado para android, que codifica mensagens.";
+                loadGalleryTitleAndDescription(title, description);
+                loadGalleryImages(tpGallery);
+                break;
+            case 'PequenosInfinitos':
+                var title = "Pequenos Infinitos";
+                var description = "Design do livro escrito por mim como trabalho de conclusão da faculdade.";
+                loadGalleryTitleAndDescription(title, description);
+                loadGalleryImages(pequenosInfinitosGallery);
+                break;
+        }
+
+        setImageDetailClickEvent();
+        $("#portfolioDetails").fadeIn(500);
+    });
+
+    
+}
+
+function setImageDetailClickEvent(){
+    $(".divGaleryDetails img").each(function (){
+        $(this).bind("click", function(){
+            $(".lightboxImg").attr('src', ($(this).attr('src')));
+        });
+    });
+}
+
+function loadGalleryTitleAndDescription(title, description){
+    $("#portfolioDetailTitle").html(title);
+    $("#portfolioDetailDescription").html(description);
+}
+
+function loadGalleryImages(galleryArray){
+    var count = 0;
+
+    addNewGalleryRow();
+
+    for (var i = 0; i < galleryArray.length; i++){
+        loadGalleryImageSrc(galleryArray[i]);
+
+        if (count == 3){
+            addNewGalleryRow();
+            count = 0;
+        }
+        else
+            count++;
+    }
+}
+
+function loadGalleryImageSrc(src){
+    var html = "<div class='col-md-3 col-sm-4 col-xs-6'>" +
+                "<img class='img-responsive' data-toggle='modal' data-target='#lightbox' src='" + src + "' />" +
+           "</div>";
+
+    $(".divGaleryDetails .row:last-child").append(html);
+}
+
+function addNewGalleryRow()
+{
+    var html = "<div class='row'></div>";
+    $(".divGaleryDetails").append(html);
+}
+
 $(function(){
     setAccordion();
+    setScrollEvent();
 })
